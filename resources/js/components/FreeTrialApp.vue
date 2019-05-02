8c837892cb115d5cd02c4d4a2879771bd78cc3b9
Joshua Anthony <jantho1990@gmail.com>
2019-05-01 23:17:45 -0500

    moved app js script tag to bottom of body where it should be; started FreeTrialApp component

---
+++ resources/js/components/FreeTrialApp.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="free-trial-app">
+        <h2>Get <strong>3 free downloads</strong> when you start your free trial today.</h2>
+        <ul class="benefits">
+            <li>Get access to our full Pro catalog</li>
+            <li>Cancel anytime, risk free</li>
+            <li>7 days free</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FreeTrialApp"
+}
+</script>
+
+<style lang="scss">
+.free-trial-app {
+    .benefits {
+        list-style-image: url('/img/checkmark-lined.svg');
+        li {
+        }
+    }
+}
+</style>
