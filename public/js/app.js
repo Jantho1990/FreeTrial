c3054a0780017601e16b98350e1a7dd6f3f8eecd
Joshua Anthony <jantho1990@gmail.com>
2019-05-02 19:34:04 -0500

    added a component name for ExistingUserPanel

--- public/js/app.js
+++ public/js/app.js
@@ -1770,7 +1770,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-/* harmony default export */ __webpack_exports__["default"] = ({});
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: 'ExistingUserPanel'
+});
 
 /***/ }),
 
